b682c6cae7aa72aefdf8303519278193feb7afe2
Jose Tomas Hernandez Lopez <joseto.lopez00@gmail.com>
2026-02-25 17:57:29 +0100

    update eslint

--- eslint.config.js
+++ eslint.config.js
@@ -12,15 +12,12 @@ export default defineConfig([
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
-      // reactHooks.configs.flat.recommended,
+      reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite
     ],
     languageOptions: {
       ecmaVersion: 2022,
       globals: globals.browser
-    },
-    plugins: {
-      'react-hooks': reactHooks
     },
     rules: {
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
